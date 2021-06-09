eebcbc513af939e6b65caa6daf79ca6cec6a5792
Johan Guse <johanguse@gmail.com>
2021-06-08 21:02:11 -0300

    Create AppToolBar with logo

--- app/src/main/java/com/example/santanderbecas/MainActivity.kt
+++ app/src/main/java/com/example/santanderbecas/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.santanderbecas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.example.santanderbecas.R
 import com.example.santanderbecas.ui.theme.SantanderBecasTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +34,45 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+
 @Composable
 fun DefaultPreview() {
     SantanderBecasTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Toolbar() {
+    TopAppBar(backgroundColor = Color.Red) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_menu),
+                contentDescription = null,
+                tint = Color.White
+            )
+            Box(
+                modifier = Modifier.padding(12.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.santander_logo),
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_notification),
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+        
     }
 }
